b62d1fd3d096b70616e181eb95e4e9bb130224ae
Andrey Tsatsyn <tsatsynav@yandex.ru>
2023-10-15 04:38:04 +0300

    task-5.js reload

--- first-project/cycle/task-5 (2).js
+++
@@ -1,7 +0,0 @@
-let sum = 0;
-for (let i = 0; i <= 100; i++) {
-  sum = sum + i;
-  //console.log(sum);
-}
-
-alert(sum);
--- first-project/cycle/task-5.js
+++ first-project/cycle/task-5.js
@@ -1,6 +1,7 @@
-let i = 0;
-sum = 0;
-while (i < 100) {
-  ++sum;
-  ++i;
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  sum = sum + i;
+  //console.log(sum);
 }
+
+alert(sum);
