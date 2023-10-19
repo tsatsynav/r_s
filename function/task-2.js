25657227b4f90cdd3f8290a33e329dbccf0e01d2
Andrey Tsatsyn <tsatsynav@yandex.ru>
2023-10-19 05:18:18 +0300

    g

--- function/task-2.js
+++ function/task-2.js
@@ -1,6 +1,11 @@
+let userNum = Number(prompt("Введите число:").trim());
+let userEvenOddSolution = (prompt("Укажите порядок расчета (четный или нечетный, если сплошной - нажмите Ок):").trim().toLowerCase());
+let evenIter = (userNum % 2) + 2;
+// const oddIter = (userNum % 2) + 1;
+// console.log(evenIter);
 function getSumOfNumbers(number, type = "") {
   let sum = 0;
-  let i = 0;
+  let i = 0;  
   if (number === Number || type === "") {
     for (i; i <= number; i++) {
       sum = sum + i;
@@ -8,11 +13,11 @@ function getSumOfNumbers(number, type = "") {
       //return sum;
     }
   } else if (number === Number || type === "even") {
-    for (i; i <= number; i++) {
-      sum = sum + i;
+    for (evenIter; evenIter <= number; evenIter++) {
+      sum = sum + (evenIter - 1);
       console.log(sum);
     }
   }
 }
 
-getSumOfNumbers(10, "even");
+getSumOfNumbers(userNum, userEvenOddSolution);
