41c1134fdd5af70ee163bbf048d485535e8c6923
Andrey Tsatsyn <tsatsynav@yandex.ru>
2023-10-20 05:59:11 +0300

    ggg

--- function/task-2.js
+++ function/task-2.js
@@ -1,23 +1,41 @@
-let userNum = Number(prompt("Введите число:").trim());
-let userEvenOddSolution = (prompt("Укажите порядок расчета (четный или нечетный, если сплошной - нажмите Ок):").trim().toLowerCase());
-let evenIter = (userNum % 2) + 2;
-// const oddIter = (userNum % 2) + 1;
-// console.log(evenIter);
-function getSumOfNumbers(number, type = "") {
+let inputNumber = Number(prompt("Введите число:").trim());
+let inputType = prompt("Введите even или odd для определения требуемого порядка пересчета чисел").trim().toLowerCase();
+// console.log(inputNumber);
+// console.log(inputType);
+function getSumOfNumbers(number, type) {
+  console.log(number);
   let sum = 0;
-  let i = 0;  
-  if (number === Number || type === "") {
-    for (i; i <= number; i++) {
+  if (type === "") {
+    for (let i = 0; i <= 10; i++) {
+      console.log(sum);
+      console.log(number);
+      console.log(i);
       sum = sum + i;
+      console.log(number);
       console.log(sum);
-      //return sum;
+      console.log(i);
+      return sum;
     }
-  } else if (number === Number || type === "even") {
-    for (evenIter; evenIter <= number; evenIter++) {
-      sum = sum + (evenIter - 1);
-      console.log(sum);
-    }
+  } else /*(number !== Number)*/ {
+    return NaN;
   }
+  /*for(let i = 0; i <= number; i++) {
+    if (type === "") {
+      console.log(i);
+      console.log(totalSum = totalSum + i);
+      return totalSum += i;
+    } else if (i % 2 !== 0 || type === "odd") {
+      console.log(2);
+      console.log(oddSum += i);
+      return oddSum += i;
+    } else if (i % 2 === 0 || type === "even") {
+      console.log(3);
+      return evenSum += i;
+    } else {
+      console.log(4);
+      return NaN;
+    }
+  }*/
 }
 
-getSumOfNumbers(userNum, userEvenOddSolution);
+alert(getSumOfNumbers(inputNumber, inputType));
